9a89c1fc59253f0f21c1198d5ec1a782f1f443c8
bcburin <cburin.bruno@gmail.com>
2022-10-11 14:27:57 -0300

    Implemented exeption handling

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,21 +1,23 @@
 #include "interpreter.h"
 
-
 interpreter::interpreter(std::istream* is, std::ostream* os)
-: context(is, os) {}
-
+    : context(is, os) {}
 
 void interpreter::run() {
-  // Print input marker
-  display(">> ");
-  // Read lines
-  std::string line;
-  while ( std::getline(*is_, line) && trim(line) != "end" ) {
-    // Interpret statement
-    auto st = statement::interpret(line, this);
-    // Execute statement
-    if(st) st->execute();
     // Print input marker
     display(">> ");
-  }
+    // Read lines
+    std::string line;
+    while (std::getline(*is_, line) && trim(line) != "end") {
+        try {
+            // Interpret statement
+            auto st = statement::interpret(line, this);
+            // Execute statement
+            if (st) st->execute();
+        } catch (std::invalid_argument& error) {
+            std::cout << "   " << error.what() << std::endl;
+        }
+        // Print input marker
+        display(">> ");
+    }
 }
