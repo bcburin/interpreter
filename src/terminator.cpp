fd474cc925485400d5a9699fa258712f86433538
Bruno Burin <cburin.bruno@gmail.com>
2023-10-28 05:02:00 -0500

    remove old project

--- src/terminator.cpp
+++
@@ -1,4 +0,0 @@
-#include "terminator.h"
-
-terminator::terminator(std::string str): statement(str) {}
-void terminator::execute() {  /* Do nothing */ }
--- src/token.cpp
+++
@@ -1,5 +0,0 @@
-#include "token.h"
-
-token::token(std::string str): str_(str) {}
-
-std::ostream& operator<<(std::ostream& os, const token& tk) { return os << tk.str(); }
